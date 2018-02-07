bbbdcf8c350a85a11d17595f948b2b699dedf965
Marcel Patyk <mpatyk2@outlook.com>
2018-02-07 14:04:08 -0600

    First Commit

---
+++ src/main/java/Model/EmpBusinessLogic.java
@@ -0,0 +1,41 @@
+package Model;
+
+public class EmpBusinessLogic {
+
+	public double calculateYearlySalary(Employee employee)
+	{
+		double yearlySalary = 0;
+		yearlySalary = employee.getMonthlySalary() * 12;
+		return yearlySalary;
+	}
+	
+	public double calculateApprasial(Employee employee)
+	{
+		double appraisal = 0;
+		
+		if (employee.getMonthlySalary() < 10000)
+		{
+			appraisal = 500;
+		}
+		else
+		{
+			appraisal = 1000;
+		}
+		
+		return appraisal;
+	}
+	
+	public boolean isRetirementAge(Employee employee)
+	{
+		int retirementAge = 65;
+		
+		if (employee.getAge() >= retirementAge)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
---
+++ src/main/java/Model/Employee.java
@@ -0,0 +1,43 @@
+package Model;
+
+public class Employee {
+	
+	private String name;
+	private double monthlySalary;
+	private int age;
+	
+	public Employee()
+	{
+		
+	}
+	
+	public Employee(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getMonthlySalary() {
+		return monthlySalary;
+	}
+
+	public void setMonthlySalary(double monthlySalary) {
+		this.monthlySalary = monthlySalary;
+	}
+	
+}
